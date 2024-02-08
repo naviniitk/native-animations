2e440fc9a49611f3a44c16e7a6db039a1595c798
Naveen Kumar <navin10102001@gmail.com>
2024-02-08 23:23:39 +0530

    added loop animation for fade in and fade out

--- app/(tabs)/native-docs.tsx
+++ app/(tabs)/native-docs.tsx
@@ -1,21 +1,51 @@
 import React, { PropsWithChildren, useEffect, useRef } from "react";
 import { Animated, Text, View, ViewStyle } from "react-native";
 
-type FadeInViewProps = PropsWithChildren<{ style: ViewStyle }>;
+type FadeInViewProps = PropsWithChildren<{ style: ViewStyle; loop?: boolean }>;
 
-const FadeInView: React.FC<FadeInViewProps> = ({ style, children }) => {
+const FadeInView: React.FC<FadeInViewProps> = ({
+  style,
+  children,
+  loop = false,
+}) => {
   const fadeInAnim = useRef(new Animated.Value(0)).current;
 
   useEffect(() => {
-    Animated.timing(fadeInAnim, {
-      toValue: 1,
-      duration: 10000,
-      useNativeDriver: true,
-    }).start();
-  }, [fadeInAnim])
+    if (loop) {
+      Animated.loop(
+        Animated.sequence([
+          Animated.timing(fadeInAnim, {
+            toValue: 1,
+            duration: 1000,
+            useNativeDriver: true,
+          }),
+          Animated.timing(fadeInAnim, {
+            toValue: 0,
+            delay: 100,
+            duration: 1000,
+            useNativeDriver: true,
+          }),
+        ])
+      ).start();
+    } else {
+      Animated.timing(fadeInAnim, {
+        toValue: 1,
+        duration: 1000,
+        useNativeDriver: true,
+      }).start();
+    }
+  }, [fadeInAnim]);
 
   return (
-    <Animated.View style={{ backgroundColor: "aqua", width: 200, height: 40, opacity: fadeInAnim, ...style }}>
+    <Animated.View
+      style={{
+        backgroundColor: "aqua",
+        width: 200,
+        height: 40,
+        opacity: fadeInAnim,
+        ...style,
+      }}
+    >
       {children}
     </Animated.View>
   );
@@ -31,7 +61,13 @@ export default function NativeDocs() {
         padding: 20,
       }}
     >
-      <FadeInView style={{borderRadius: 10, alignItems: 'center', justifyContent: 'center'}}>
+      <FadeInView
+        style={{
+          borderRadius: 10,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
         <Text>Hello World</Text>
       </FadeInView>
     </View>
